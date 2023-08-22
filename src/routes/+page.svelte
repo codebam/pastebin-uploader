b72caf11bd6ede6e2d029fe2f9123cec0c626869
Sean Behan <codebam@riseup.net>
2023-08-22 17:19:03 -0400

    add uploader page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="container">
+<h1>Sean's Pastebin</h1>
+<p>After uploading you will be redirected to yourfile.txt. Make sure to
+save the link if you're encrypting. You can change .txt to any valid MIME
+type to display it in the browser.</p>
+<h2>plain text</h2>
+<form method="post" enctype="multipart/form-data">
+<div>
+<textarea name="paste" rows="20" cols="30" id="paste"></textarea>
+</div>
+<button>submit</button>
+<h2>plain file</h2>
+</form>
+<form method="post" enctype="multipart/form-data">
+<div>
+<input type="file" id="upload" name="upload">
+</div>
+<button>submit</button>
+</form>
+<h2>encrypted text</h2>
+<form method="post" action="./encrypt" enctype="multipart/form-data">
+<div>
+<textarea name="paste" rows="20" cols="30" id="paste"></textarea>
+</div>
+<button>submit</button>
+</form>
+<h2>encrypted file</h2>
+<form method="post" action="./encrypt" enctype="multipart/form-data">
+<div>
+<input type="file" id="upload" name="upload">
+</div>
+<button>submit</button>
+</form>
+</div>
